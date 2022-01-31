cf6b70e44a9461461e670f78741f8cf18feb25c8
SAMSUNG <SAMSUNG@SAMSUNG-LAP>
2022-01-31 18:12:38 +0530

    this is the commit.....

---
+++ src/com/automate/ModelGit.java
@@ -0,0 +1,10 @@
+package com.automate;
+
+public class ModelGit {
+	public static void main(String[] args) {
+		String Value= "Git is the tool belongs to automation....";
+		
+		System.out.println(Value);
+	}
+
+}
