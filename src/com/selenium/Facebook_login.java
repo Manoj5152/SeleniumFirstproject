90199072cbaad3f3d8f1030002501d5fda715e48
SAMSUNG <SAMSUNG@SAMSUNG-LAP>
2022-01-31 15:14:33 +0530

    first commit....

--- src/com/selenium/Facebook_login.java
+++ src/com/selenium/Facebook_login.java
@@ -16,7 +16,7 @@ public class Facebook_login {
 	d.findElement(By.name("pass")).sendKeys("07373208125");
 	d.findElement(By.name("login")).click();
 	Thread.sleep(5000);
-	d.findElement(By.className("oajrlxb2 g5ia77u1 qu0x051f esr5mh6w e9989ue4 r7d6kgcz rq0escxv nhd2j8a9 nc684nl6 p7hjln8o kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x jb3vyjys rz4wbd8a qt6c0cv9 a8nywdso i1ao9s8h esuyzwwr f1sip0of lzcic4wl gmql0nx0 gpro0wi8 hnhda86s")).click();
+	d.findElement(By.className("a8c37x1j ni8dbmo4 stjgntxs l9j0dhe7")).click();
 	 }
 
 }
